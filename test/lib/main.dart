61e15c877de82d7fe3b6288a96fc74542ddf1f59
Mika kaudewitz <kaudewitzmika@gmail.com>
2021-11-04 14:01:30 +0100

    test

--- test/lib/main.dart
+++ test/lib/main.dart
@@ -8,14 +8,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Wilkommen"),
-        ),
-        body: Center(
-          child: Text(SuperHero.random()),
-        ),
+      home: SuperHeroNameGenerator());
+  }
+}
+
+class SuperHeroNameGenerator extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _SuperHeroName();
+}
+
+class _SuperHeroName extends State<SuperHeroNameGenerator> {
+  final _SuperHeroName = <String>[];
+  final _hearted = <String>{} ;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Wilkommen"),
       ),
+      body: Widget _buildBody(),
     );
   }
+
+  Widget _buildBody() {
+    return ListView.builder(
+        padding, itemBuilder: (BuildContext context, int index) {  },: Edgeninsets.all(16)
+    itemBuilder: (context, i) {
+          if(i.isOdd){
+            return Divider();
+          }
+          if(i ~/ 2 >= _SuperHeroName.length){
+            _SuperHeroName.addAll(_generateSuperheroNames()):
+          }
+          return _buildRow(_SuperHeroName[i ~/ 2]);
+    }
+    );
+  }
+
+
 }
